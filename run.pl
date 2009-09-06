08df125b643f159c0b85c004e6c3c10faaa6e15c
Orion Letizi <oletizi@mac.com>
2009-09-05 22:48:57 -0700

    again, not sure why some changed files were not added to the commit.  Trying the -a flag to commit

--- run.pl
+++ run.pl
@@ -9,37 +9,43 @@ my $SRC = $HOMEDIR . "/src/main/java";
 my $TARGET = $HOMEDIR . "/target";
 my $JAVA = "java";
 my $JAVA_OPTS = "";
-my $CLASSPATH = configureClasspath(qw/antlr-2.7.6.jar commons-collections-3.1.jar dom4j-1.6.1.jar hsqldb.jar javassist-3.9.0.GA.jar jta-1.1.jar slf4j-api-1.5.8.jar/);
+my $BUILD_JAR = "$TARGET/terracotta-hibernate-sample.jar";
+unless (-e $BUILD_JAR) {
+  print "The build jar doesn't exist: $BUILD_JAR.  Have you run 'ant dist.jar' yet?\n" and exit(1);
+}
+my $CLASSPATH = configureClasspath(qw/antlr-2.7.6.jar commons-collections-3.1.jar dom4j-1.6.1.jar ehcache-1.6.2.jar hibernate3.jar hsqldb.jar javassist-3.9.0.GA.jar jta-1.1.jar log4j-1.2.15.jar slf4j-api-1.5.8.jar slf4j-log4j12-1.5.8.jar/);
 
 my %commands = (
-		'jdbc' => "org.terracotta.tutorial.JDBCTest"
+		'jdbc' => "org.terracotta.tutorial.JDBCTest",
+		'hibernate' => "org.hibernate.tutorial.EventManager"
 	       );
 
-my $command = parseArgs(@ARGV);
+my ($command, @args) = parseArgs(@ARGV);
 
 print usage() and exit(1) unless $command;
 
 print "my command: $command\n";
 
 if ($commands{$command}) {
-  runJava($commands{$command});
+  runJava($commands{$command}, @args);
 } else  {
   print usage() and exit(1);
 }
 
 sub runJava {
-  my ($class) = @_;
-  my $cmd = "$JAVA $JAVA_OPTS -cp $CLASSPATH $class";
+  my ($class, @args) = @_;
+  my $argstring = join(' ', @args);
+  my $cmd = "$JAVA $JAVA_OPTS -cp $CLASSPATH $class $argstring";
   print "executing $cmd...\n";
   print `$cmd`;
 }
 
 sub parseArgs {
-  my $rv = "";
   if (scalar @_) {
-    $rv = $_[0];
+    return @_;
+  } else {
+    return ("__UNKNOWN__");
   }
-  return $rv;
 }
 
 sub configureClasspath {
@@ -50,7 +56,7 @@ sub configureClasspath {
     foreach (@jarnames) {
 	push @jars, "lib/" . $_;
     }
-    push @jars, "target/";
+    push @jars, $BUILD_JAR;
     return join (':', @jars);
     
 }
--- src/main/java/org/hibernate/tutorial/EventManager.java
+++ src/main/java/org/hibernate/tutorial/EventManager.java
@@ -9,26 +9,42 @@ import org.hibernate.tutorial.util.HibernateUtil;
 
 public class EventManager {
 
-    public static void main(String[] args) {
-        EventManager mgr = new EventManager();
-
-        if (args[0].equals("store")) {
-            mgr.createAndStoreEvent("My Event", new Date());
-        }
-
-        HibernateUtil.getSessionFactory().close();
+  public static void main(String[] args) {
+    EventManager mgr = new EventManager();
+    
+    if (args[0].equals("store")) {
+      mgr.createAndStoreEvent("My Event", new Date());
     }
-
-    private void createAndStoreEvent(String title, Date theDate) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-
-        Event theEvent = new Event();
-        theEvent.setTitle(title);
-        theEvent.setDate(theDate);
-        session.save(theEvent);
-
-        session.getTransaction().commit();
+    else if (args[0].equals("list")) {
+      List events = mgr.listEvents();
+      for (int i = 0; i < events.size(); i++) {
+	Event theEvent = (Event) events.get(i);
+	System.out.println(
+	  "Event: " + theEvent.getTitle() + " Time: " + theEvent.getDate()
+	  );
+      }
     }
-
+    HibernateUtil.getSessionFactory().close();
+  }
+  
+  private List listEvents() {
+    Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+    session.beginTransaction();
+    List result = session.createQuery("from Event").list();
+    session.getTransaction().commit();
+    return result;
+  }
+  
+  private void createAndStoreEvent(String title, Date theDate) {
+    Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+    session.beginTransaction();
+    
+    Event theEvent = new Event();
+    theEvent.setTitle(title);
+    theEvent.setDate(theDate);
+    session.save(theEvent);
+    
+    session.getTransaction().commit();
+  }
+  
 }
